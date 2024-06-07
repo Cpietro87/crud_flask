40f729f517ff6f83fbcf3b78417c58da7ad95db9
Cpietro87 <cristianpietrobon20192@gmail.com>
2024-06-07 20:12:38 -0300

    update

--- server.py
+++ server.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, redirect, render_template, request, url_for
 from flask_mysqldb import MySQL
 from dotenv import load_dotenv
 import os
@@ -26,6 +26,34 @@ def data():
     cur.execute('SELECT * FROM contact')
     data = cur.fetchall()
     return render_template( 'index.html', clientes = data)
+
+
+@app.route('/edit/<id>')
+def get_client(id):
+    cur = mysql.connection.cursor()
+    cur.execute('SELECT * FROM contact WHERE id = %s', (id,))
+    data = cur.fetchall()
+    cur.close()
+    print(data[0])
+    return render_template('edit-cliente.html',contact= data[0])
+
+@app.route('/update/<id>', methods=['POST'])
+def update_contact(id):
+    if request.method == 'POST':
+        fullname = request.form['fullname']
+        phone = request.form['phone']
+        email = request.form['email']
+        cur = mysql.connection.cursor()
+        cur.execute("""
+            UPDATE contact
+            SET fullname = %s,
+                email = %s,
+                phone = %s
+            WHERE id = %s
+        """, (fullname, email, phone, id))
+        mysql.connection.commit()
+        return redirect(url_for('data')) 
+
 
 
 # Server

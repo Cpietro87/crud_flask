f99fed9ec97b967ba4d7fd49b17cf2355eff7efb
Cpietro87 <cristianpietrobon20192@gmail.com>
2024-06-07 19:07:41 -0300

    read datos y vista

--- server.py
+++ server.py
@@ -1,11 +1,33 @@
-from flask import Flask
+from flask import Flask, render_template
+from flask_mysqldb import MySQL
+from dotenv import load_dotenv
+import os
 
 
-app = Flask(__name__)
+app = Flask(__name__,  static_url_path= '/static' )
+load_dotenv()
+# Conección MySQL
+app.config['MYSQL_USER'] = os.getenv('MYSQL_USER')
+app.config['MYSQL_PASSWORD'] = os.getenv('MYSQL_PASSWORD')
+app.config['MYSQL_HOST'] = os.getenv('MYSQL_HOST') 
+app.config['MYSQL_DB'] = os.getenv('MYSQL_DB') 
 
-@app.route('/')
-def index():
-    return 'Hola Mundo!!!'
+mysql = MySQL(app)
 
+#Rutas
+
+@app.route('/login')
+def main():
+    return render_template('login.html')
+
+@app.route('/')# App Web
+def data():
+    cur = mysql.connection.cursor()
+    cur.execute('SELECT * FROM contact')
+    data = cur.fetchall()
+    return render_template( 'index.html', clientes = data)
+
+
+# Server
 if __name__ == '__main__':
-    app.run(port=5000)
+    app.run(port=5000, debug=True)
